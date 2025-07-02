af80cfdf56ac260b790398fa9ec9fd4ac4520575
rafaelluiz <dev.rafaa@gmail.com>
2025-07-02 20:20:04 -0300

    Ajustando responsividade e criando e estilizando página de carrinho

--- src/pages/carrinho/index.tsx
+++ src/pages/carrinho/index.tsx
@@ -1,7 +1,26 @@
-
+import { useState } from "react"
+import dogCart from '../../assets/dog_cart.png'
+import { Link } from "react-router";
+import { Footer } from "../../components/footer";
 
 export function Carrinho(){
+
+    const [cart, setCart] = useState(null);
+
     return(
-        <h1>Ops! Seu carrinho está vázio!</h1>
+        <main>
+            <section className="w-full flex flex-col items-center justify-center">
+                {!cart && (
+                    <div className="w-full  flex flex-col items-center justify-center h-screen ">
+                        <img src={dogCart} alt="cachorrinho" />
+                        <h1>Que pena! Seu carrinho está vazio!</h1> 
+                        <Link className="border-[1px] border-baseMenuText p-2 rounded-md shadow-md shadow-baseMenuText/75 text-baseMenuText font-bold mt-2.5 hover:bg-baseMenuText hover:text-white hover:duration-300" to='/'>Voltar ao Início</Link>
+                    </div>
+                )}
+
+               
+            <Footer/>
+            </section>
+        </main> 
     )
 }
--- src/pages/pageDog/index.tsx
+++ src/pages/pageDog/index.tsx
@@ -102,7 +102,7 @@ export function PageDog() {
                  </section>
 
                 <section className="  w-full">
-                    <div className="mt-8  ">
+                    <div className="mt-8 ">
                         <div className="absolute left-0  bg-[url(../../src/assets/cardimage.png)] bg-cover bg-blend-overlay bg-[#3fffdf] w-auto px-8 py-2 rounded-r-full shadow-[#1C653C] shadow-xs z-10  md:px-14">
                             <h4 className="flex items-center justify-center gap-2 px-12 text-[#1C653C] font-mochiy text-xs">Rações</h4>
                         </div>
